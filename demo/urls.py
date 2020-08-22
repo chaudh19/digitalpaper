2fe09b141dbf6c192552ad82c2a7dfed8f3494c3
simran <simran.d.chaudhry@gmail.com>
2020-08-22 15:51:22 -0700

    image to equation

--- demo/urls.py
+++ demo/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('save_image', views.save_image, name='save_image'),
+    path('display_result', views.display_result, name='display_result'),
 ]
